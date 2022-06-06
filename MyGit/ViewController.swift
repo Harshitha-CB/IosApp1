7ddbca514a78ad4a8448dd23f1dd70229be297aa
Harshitha-CB <harshithaneygar@gmail.com>
2022-06-06 01:46:50 -0700

    Initial Commit

---
+++ MyGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGit
+//
+//  Created by Capgemini_DA078 on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
